d785e08c2a9c9ee24a550120a9dcb4907c8138c7
jakwings <jakwings@gmail.com>
2013-11-24 19:33:41 +0800

    update

--- js/blog/config.js
+++ js/blog/config.js
@@ -99,6 +99,7 @@ define('blog.config', function () {
       //  container.className += ' ds-thread';
       //  container.setAttribute('data-thread-key', id);
       //  container.setAttribute('data-url', url);
+      //  container.setAttribute('data-title', document.title);
       //  var ds = document.createElement('script');
       //  ds.type = 'text/javascript';ds.async = true;
       //  ds.src = 'http://static.duoshuo.com/embed.js';
@@ -112,6 +113,7 @@ define('blog.config', function () {
       //  clone.className = 'comments hide';
       //  clone.setAttribute('data-thread-key', id);
       //  clone.setAttribute('data-url', url);
+      //  clone.setAttribute('data-title', document.title);
       //  container.parentNode.replaceChild(clone, container);
       //  window.DUOSHUO.EmbedThread(clone);
       //}
@@ -125,6 +127,7 @@ define('blog.config', function () {
         window.disqus_disable_mobile = false;
         window.disqus_identifier = id;
         window.disqus_url = url;
+        window.disqus_title = document.title;
         container.id = 'disqus_thread';
         var dsq = document.createElement('script');
         dsq.type = 'text/javascript';
@@ -138,6 +141,7 @@ define('blog.config', function () {
           config: function () {
             this.page.identifier = id;
             this.page.url = url;
+            this.page.title = document.title;
           }
         });
       }
